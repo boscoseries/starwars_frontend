a7c8d87da07a775596bdcff28cef945b415af5c1
Johnbosco <boscoseries@gmail.com>
2019-08-30 17:23:18 +0100

    add starwars logo to header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,6 +4,8 @@ import { CardType2 } from "../Card";
 import "./styles.css";
 
 import logo from "../../images/hero-banner.jpg";
+import starWars from "../../images/swarslogo.png";
+import starWarsdirectory from "../../images/swarsdirectory.png";
 
 export default function Header () {
   return (
@@ -12,11 +14,17 @@ export default function Header () {
         imageSrc={logo}
         imageClass="card-img"
         cardClass="card-img-overlay"
+        cardTitle={<img src={starWars} alt="star-wars-logo" width="130px" />}
         cardTitleClass="card_title"
         cardText={
           <div className="d-flex-column justify-content-center align-items-center">
             <p className="d-flex justify-content-center">
+              <img src={starWarsdirectory} alt="star-wars-logo" width="320px" />
             </p>
+            <div className="header_text">
+              Find your favourite Characters, Films, Species, <br />
+              <span> Starships and Planets </span>
+            </div>
           </div>
         }
         cardTextClass="card_text"
