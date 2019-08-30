788ad5c6ae45d74da10731d8e872b238714e535c
Johnbosco <boscoseries@gmail.com>
2019-08-30 17:12:54 +0100

    add arrow symbol to button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 
 export default function Button ({ butonType, buttonClass, buttonTag }) {
@@ -6,6 +8,7 @@ export default function Button ({ butonType, buttonClass, buttonTag }) {
     <button type={butonType} className={buttonClass} style={{ fontSize: "13px", padding: "12px 30px", background: "#D8D8D8" }}>
       {buttonTag}
       <span className="ml-2">
+        <FontAwesomeIcon icon={faArrowRight} />
       </span>
     </button>
   );
