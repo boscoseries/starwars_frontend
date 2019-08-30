5d334e0783a6aba61b7ef7e653683c27e22e7c24
Johnbosco <boscoseries@gmail.com>
2019-08-30 17:20:35 +0100

    mount header component with hero-banner image background

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Homepage from "./pages/Homepage";
+import Header from "./components/Header";
 
 function App () {
   return (
     <Router>
       <div>
+        <Header />
         <Route path="/" exact component={Homepage} />
       </div>
     </Router>
