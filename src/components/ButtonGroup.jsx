4f7cb4556507b5965b58a05ee1cea4096e5916ed
Johnbosco <boscoseries@gmail.com>
2019-09-05 07:50:11 +0100

    refactor pagination code to load next page onClick

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -3,17 +3,17 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
-export default function ButtonGroup ({ onClick, initial, final, total }) {
+export default function ButtonGroup({ clickNext, clickPrev, pageA, pageB, pageTotal }) {
   return (
     <div className="btn-group" style={{ width: "15%" }} role="group" aria-label="Basic example">
       <div className="d-flex justify-content-center align-items-center text-black-50">
-        {initial} - {final} of {total}
+        {pageA} - {pageB} of {pageTotal}
       </div>
       <div className="d-flex">
-        <button type="button" className="btn btn-light" id="pgbtn" onClick={onClick}>
+        <button type="button" className="btn btn-light" id="pgbtn" onClick={clickPrev}>
           <FontAwesomeIcon style={{ fontSize: "1.3rem" }} icon={faAngleLeft} />
         </button>
-        <button type="button" className="btn btn-light" id="pgbtn2" onClick={onClick}>
+        <button type="button" className="btn btn-light" id="pgbtn2" onClick={clickNext}>
           <FontAwesomeIcon style={{ fontSize: "1.3rem" }} icon={faAngleRight} />
         </button>
       </div>
--- src/pages/Starships.jsx
+++ src/pages/Starships.jsx
@@ -18,13 +18,19 @@ const corsPass = "https://cors-anywhere.herokuapp.com/";
 export default function Starships() {
   const [ships, setShips] = useState([]);
   const [loading, setLoading] = useState(true);
-  let [pageNum, setPageNum] = useState(1);
+  let [pageA, setPageA] = useState(1);
+  let [pageB, setPageB] = useState(0);
 
   // function to fetch starships from SWAPI
   const fetchStarships = async () => {
-    const result = await swapi.get(`${corsPass}https://swapi.co/api/starships/?page=${pageNum}`);
+    const result = await swapi.get(`${corsPass}https://swapi.co/api/starships/?page=${pageA}`);
+    if (result) {
     setShips(result);
-    setLoading(false);
+      const maxListing = (pageA - 1) * 10 + result.results.length;
+      let valueB = maxListing > ships.count ? ships.count : maxListing;
+      setPageB(valueB);
+      setLoading(false);
+    }
   };
 
   // separate starship images from assets
@@ -39,61 +45,68 @@ export default function Starships() {
   const nextPage = e => {
     e.preventDefault();
     if (!loading) {
-      setPageNum(pageNum++);
+      const maxPage = Math.ceil(ships.count / 10);
+      let valueA = pageA >= maxPage ? maxPage : pageA += 1;
+      setPageA(valueA);
+      const maxListing = (pageA - 1) * 10 + ships.results.length;
+      let valueB = maxListing > ships.count ? ships.count : maxListing;
+      setPageB(valueB);
     }
+  };
+
+  // function runs onclick of previous bubbon
+  const prevPage = e => {
+    // !loading && maxListing > ships.count ? ships.count : maxListing
+    // e.preventDefault();
+    // if (!loading) {
+    //   setPageA(pageB--);
+    // }
   };
 
   // fires content on event change
   useEffect(() => {
     fetchStarships();
-  }, [pageNum]);
-
-  // maximum listings
-  const maxListing = !loading ? (pageNum - 1) * 10 + ships.results.length : "";
-  // maximum no of pages
-  const maxPage = Math.ceil(ships.count / 10);
+  }, [pageA]);
 
   return (
-    <div>
-      <Title containerClass="mt-5 pt-2 mb-5 pb-2" titleClass="title" title="Popular Starships" lineClass="hr" />
-      <div className="d-flex row flex-wrap" style={{ justifyContent: "center" }}>
-        {!loading &&
-          ships.results.map((result, index) => {
-            const num = Math.floor(Math.random() * Math.floor(6));
-            const { name, model, cargo_capacity } = result;
-            return (
-              <CardType2
-                key={index}
-                imageSrc={starshipImages[num]}
-                imageClass="card-img-top"
-                altText="image"
-                cardWidth={{ width: "360px" }}
-                cardClass="card-body"
-                cardTitleClass="card-title d-flex justify-content-center"
-                cardTextClass="card-text"
-                cardTitle={name}
-                cardText={`Model: ${model}`}
-                cardText2={`Cargo Capacity: ${cargo_capacity}`}
-                buttonType="button"
-                buttonFlexStyle="d-flex justify-content-end"
-                buttonTag="Read More"
-                buttonClass="btn btn-secondary btn-lg font-weight-lighter rounded-0 btn-light"
-                buttonStyles={{ fontSize: "13px", padding: "12px 30px", background: "#D8D8D8" }}
-                buttonIcon={<FontAwesomeIcon icon={faArrowRight} />}
-              />
-            );
-          })
-        }
-      </div>
-      <div className="d-flex justify-content-center mt-5 pt-2 mb-5 pb-2">
-        <ButtonGroup
-          onClick={nextPage}
-          id={"pgbtn"}
-          initial={pageNum > maxPage ? maxPage : pageNum}
-          final={!loading && maxListing > ships.count ? ships.count : maxListing}
-          total={ships.count}
-        />
-      </div>
-    </div>
+    <React.Fragment>
+      {!loading ? (
+        <>
+          <Title containerClass="mt-5 pt-2 mb-5 pb-2" titleClass="title" title="Popular Starships" lineClass="hr" />
+          <div className="d-flex row flex-wrap" style={{ justifyContent: "center" }}>
+            {ships.results.map((result, index) => {
+              const num = Math.floor(Math.random() * Math.floor(6));
+              const { name, model, cargo_capacity } = result;
+              return (
+                <CardType2
+                  key={index}
+                  imageSrc={starshipImages[num]}
+                  imageClass="card-img-top"
+                  altText="image"
+                  cardWidth={{ width: "360px" }}
+                  cardClass="card-body"
+                  cardTitleClass="card-title d-flex justify-content-center"
+                  cardTextClass="card-text"
+                  cardTitle={name}
+                  cardText={`Model: ${model}`}
+                  cardText2={`Cargo Capacity: ${cargo_capacity}`}
+                  buttonType="button"
+                  buttonFlexStyle="d-flex justify-content-end"
+                  buttonTag="Read More"
+                  buttonClass="btn btn-secondary btn-lg font-weight-lighter rounded-0 btn-light"
+                  buttonStyles={{ fontSize: "13px", padding: "12px 30px", background: "#D8D8D8" }}
+                  buttonIcon={<FontAwesomeIcon icon={faArrowRight} />}
+                />
+              );
+            })}
+          </div>
+          <div className="d-flex justify-content-center mt-5 pt-2 mb-5 pb-2">
+            <ButtonGroup clickPrev={prevPage} clickNext={nextPage} pageA={pageA} pageB={pageB}
+            pageTotal={ships.count}
+            />
+          </div>
+        </>
+      ) : null}
+    </React.Fragment>
   );
 }
